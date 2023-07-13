0829bbe77f25ed41454705a9f3bea774753eb9e0
Amal911 <amaleattath911@gmail.com>
2023-07-13 21:23:01 +0530

    name updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,12 +43,12 @@ addEventOnElem(navLinks, "click", closeNavbar);
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[data-back-top-btn]");
 
-window.addEventListener("scroll", function () {
-  if (window.scrollY >= 100) {
-    header.classList.add("active");
-    backTopBtn.classList.add("active");
-  } else {
-    header.classList.remove("active");
-    backTopBtn.classList.remove("active");
-  }
-});
+// window.addEventListener("scroll", function () {
+//   if (window.scrollY >= 100) {
+//     header.classList.add("active");
+//     backTopBtn.classList.add("active");
+//   } else {
+//     header.classList.remove("active");
+//     backTopBtn.classList.remove("active");
+//   }
+// });
